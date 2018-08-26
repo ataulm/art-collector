a41e9c9ff4a7fc57633637912ef7fad82748ff6e
Ataul Munim <hello@ataulm.com>
2018-08-26 16:14:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+no-name-yet
+===========
+
+https://github.com/harvardartmuseums/api-docs
+
+#### Aim
+**tiny** modularised app, showcasing applinks, deeplinks and instant apps.
+
+#### Current state (inc. next steps if applicable)
+vaporware.
---
+++ app/src/main/java/com/example/MyActivity.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my)
+    }
+}
