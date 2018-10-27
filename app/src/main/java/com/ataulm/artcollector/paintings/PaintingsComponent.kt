f09f557ee883376aeb60e257dfc8812447f615b8
Ataul Munim <hello@ataulm.com>
2018-10-27 18:46:15 +0100

    Remove unnecessary activity on the Component

--- app/src/main/java/com/ataulm/artcollector/paintings/PaintingsComponent.kt
+++ app/src/main/java/com/ataulm/artcollector/paintings/PaintingsComponent.kt
@@ -1,7 +1,6 @@
 package com.ataulm.artcollector.paintings
 
 import com.ataulm.artcollector.paintings.ui.PaintingsActivity
-import dagger.BindsInstance
 import dagger.Component
 
 @Component(modules = [PaintingsModule::class])
@@ -12,9 +11,6 @@ internal interface PaintingsComponent {
     @Component.Builder
     interface Builder {
 
-        @BindsInstance
-        fun activity(activity: PaintingsActivity): Builder
-
         fun paintingsModule(module: PaintingsModule): Builder
 
         fun build(): PaintingsComponent
@@ -23,7 +19,6 @@ internal interface PaintingsComponent {
 
 internal fun PaintingsActivity.injectDependencies() {
     DaggerPaintingsComponent.builder()
-            .activity(this)
             .build()
             .inject(this)
 }
