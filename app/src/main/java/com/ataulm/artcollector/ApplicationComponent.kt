5179e03e6d52e091ddb8c5f3418cb82bf9b4480c
Ataul Munim <hello@ataulm.com>
2018-11-08 11:55:26 -0800

    Remove unnecessary inject method on ApplicationComponent
    - we don't use this component to directly inject anything
    - instead, we use this as a "parent" component to feature modules

--- app/src/main/java/com/ataulm/artcollector/ApplicationComponent.kt
+++ app/src/main/java/com/ataulm/artcollector/ApplicationComponent.kt
@@ -1,14 +1,11 @@
 package com.ataulm.artcollector
 
-import android.app.Application
 import com.squareup.picasso.Picasso
 import dagger.BindsInstance
 import dagger.Component
 
 @Component(modules = [ApplicationModule::class])
 interface ApplicationComponent {
-
-    fun inject(application: Application)
 
     fun picasso(): Picasso
 
