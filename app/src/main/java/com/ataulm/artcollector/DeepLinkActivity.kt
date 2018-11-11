b4ec7656512f8d58f0b0a1474e569870d39e2427
Ataul Munim <hello@ataulm.com>
2018-11-10 21:25:01 -0800

    Adds Deep Link support for the three screens

---
+++ app/src/main/java/com/ataulm/artcollector/DeepLinkActivity.kt
@@ -0,0 +1,32 @@
+package com.ataulm.artcollector
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+private const val ARTIST = 0
+private const val PAINTING = 1
+
+/**
+ * Supports http/https:
+ *
+ * - art-collector.ataulm.com
+ * - art-collector.ataulm.com/{artist-id}
+ * - art-collector.ataulm.com/{artist-id}/{painting-id}
+ */
+class DeepLinkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val uri = getIntentUri()
+        val segments = uri.pathSegments
+        val intent = when (uri.pathSegments.size) {
+            2 -> paintingIntent(segments[ARTIST], segments[PAINTING])
+            1 -> artistGalleryIntent(segments[ARTIST])
+            else -> galleryIntent()
+        }
+        startActivity(intent)
+        finish()
+    }
+
+    private fun getIntentUri() = intent.data!! // only way to open this activity
+}
