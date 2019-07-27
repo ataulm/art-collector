5242f6f4ae72194ebc160116a6a65e61ae5e4ffb
Ataul Munim <hello@ataulm.com>
2019-07-27 12:11:31 +0100

    Move appmodule to appcomponent so it's easier to read
    - while re-learning dagger :sweat_smile:

--- app/src/main/java/com/ataulm/artcollector/ApplicationComponent.kt
+++ app/src/main/java/com/ataulm/artcollector/ApplicationComponent.kt
@@ -1,8 +1,16 @@
 package com.ataulm.artcollector
 
+import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.readystatesoftware.chuck.ChuckInterceptor
 import dagger.BindsInstance
 import dagger.Component
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 @Component(modules = [ApplicationModule::class])
 interface ApplicationComponent {
@@ -18,5 +26,28 @@ interface ApplicationComponent {
         fun application(application: ArtCollectorApplication): Builder
 
         fun build(): ApplicationComponent
+    }
+}
+
+@Module
+object ApplicationModule {
+
+    @JvmStatic
+    @Provides
+    fun glideRequestManager(application: ArtCollectorApplication): RequestManager = Glide.with(application)
+
+    @JvmStatic
+    @Provides
+    fun harvardArtMuseumApi(application: ArtCollectorApplication): HarvardArtMuseumApi {
+        return Retrofit.Builder()
+                .client(OkHttpClient.Builder()
+                        .addNetworkInterceptor(AddApiKeyQueryParameterInterceptor(BuildConfig.HARVARD_KEY))
+                        .addInterceptor(ChuckInterceptor(application))
+                        .build())
+                .baseUrl(HarvardArtMuseumApi.ENDPOINT)
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+                .create(HarvardArtMuseumApi::class.java)
     }
 }
